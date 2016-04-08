e8b2bdf8e3d680bdeeb1ca6d2ff591735e55eefc
pro-tsato <t.sato@pro-japan.co.jp>
2016-04-08 13:34:20 +0900

    [SampleJava]緯度経度に関するユーティリティクラス 引数使ってなかった。。

--- src/LatLngUtility.java
+++ src/LatLngUtility.java
@@ -27,17 +27,16 @@ public class LatLngUtility {
 	public static final double EARTH_DIAMETER = 6378.137;
 
 	/**
-	 * 指定緯度（lat）と、距離（distance）より、距離あたりの度数（緯度）を返す。
+	 * 距離（distance）より、距離あたりの度数（緯度）を返す。
 	 * <p>【参考サイト】
 	 * <ul>
 	 * <li>距離1kmあたりの緯度・経度の度数を計算（日本・北緯35度） | EasyRamble
 	 *  - http://easyramble.com/latitude-and-longitude-per-kilometer.html</li>
 	 * </ul>
-	 * @param lat 緯度
 	 * @param distance 距離（km）
 	 * @return 距離あたりの度数（緯度）
 	 */
-	public static double getLatDistanceAngle(double lat, int distance) {
+	public static double getLatDistanceAngle(int distance) {
 		
 		// 円周＝半径×２×π
 		double cir = EARTH_DIAMETER * (double)2 * Math.PI;
@@ -52,18 +51,17 @@ public class LatLngUtility {
 	}
 	
 	/**
-	 * 現在緯度（lat）と、経度（lng）と、距離（distance）より、距離あたりの度数（経度）を返す。
+	 * 現在緯度（lat）と、距離（distance）より、距離あたりの度数（経度）を返す。
 	 * <p>【参考サイト】
 	 * <ul>
 	 * <li>距離1kmあたりの緯度・経度の度数を計算（日本・北緯35度） | EasyRamble
 	 *  - http://easyramble.com/latitude-and-longitude-per-kilometer.html</li>
 	 * </ul>
 	 * @param lat 緯度
-	 * @param lng 経度
 	 * @param distance 距離（km）
 	 * @return 距離あたりの度数（経度）
 	 */
-	public static double getLngDistanceAngle(double lat, double lng, int distance) {
+	public static double getLngDistanceAngle(double lat, int distance) {
 		
 		// 任意の緯度あたりの円周＝任意の緯度の半径（cos×半径）×２×π
 		double cir = (Math.cos(lat * Math.PI / (double)180) * EARTH_DIAMETER) * (double)2 * Math.PI;
@@ -84,15 +82,15 @@ public class LatLngUtility {
 	public static void main(String[] args) {
 		
 		// 現在緯度と、距離より、緯度を出す。
-		System.out.println(getLatDistanceAngle(26.212432, 1));
+		System.out.println(26.212432 + getLatDistanceAngle(1));
 		
 		// 現在緯度、経度と距離より±経度を出す。
-		System.out.println(getLatDistanceAngle(26.212432, 1) + "," + getLngDistanceAngle(26.212432, 127.679200, 1));
+		System.out.println((26.212432 + getLatDistanceAngle(1)) + "," + (127.679200 + getLngDistanceAngle(26.212432, 1)));
 		
 		// 現在緯度と、距離より、緯度を出す。
-		System.out.println(getLatDistanceAngle(69.126739, 1));
+		System.out.println(69.126739 + getLatDistanceAngle(1));
 		
 		// 現在緯度、経度と距離より±経度を出す。
-		System.out.println(getLatDistanceAngle(69.126739, 1) + "," + getLngDistanceAngle(69.126739, 141.687967, 1));
+		System.out.println((69.126739 + getLatDistanceAngle(1)) + "," + (141.687967 + getLngDistanceAngle(69.126739, 1)));
 	}
 }
