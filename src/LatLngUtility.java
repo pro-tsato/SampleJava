b64c78f11dc898035f0e55f379dfe3ef73ae042e
pro-tsato <t.sato@pro-japan.co.jp>
2016-04-12 12:14:43 +0900

    [SampleJava]緯度経度に関するユーティリティクラス javadoc修正

--- src/LatLngUtility.java
+++ src/LatLngUtility.java
@@ -4,26 +4,33 @@
  * <p>【参考資料】
  * <ul>
  * <li>距離1kmあたりの緯度・経度の度数を計算（日本・北緯35度） | EasyRamble
- *  - http://easyramble.com/latitude-and-longitude-per-kilometer.html</li>
+ *  | <a href="http://easyramble.com/latitude-and-longitude-per-kilometer.html">http://easyramble.com/latitude-and-longitude-per-kilometer.html</a></li>
  * </ul>
  * 
  * <p>【動作確認用ツール】
  * <ul>
  * <li>緯度経度から地図化|Google Maps API v3を使ったポイント地図化|谷謙二研究室
- *  - http://ktgis.net/gcode/lonlatmapping.html</li>
+ *  | <a href="http://ktgis.net/gcode/lonlatmapping.html">http://ktgis.net/gcode/lonlatmapping.html</a></li>
  * <li>ShowGoogleMap
- *  - http://www.motohasi.net/GPS/ShowGoogleMap.php</li>
+ *  | <a href="http://www.motohasi.net/GPS/ShowGoogleMap.php">http://www.motohasi.net/GPS/ShowGoogleMap.php</a></li>
  * <li>２地点間の距離と方位角 - 高精度計算サイト
- *  - http://keisan.casio.jp/exec/system/1257670779</li>
+ *  | <a href="http://keisan.casio.jp/exec/system/1257670779">http://keisan.casio.jp/exec/system/1257670779</a></li>
  * <li>住所や地名から緯度・経度を一括で取得するツール | ScrapEngineer
- *  - http://scrap.php.xdomain.jp/create_tool/get_lanlng_get_address/</li>
+ *  | <a href="http://scrap.php.xdomain.jp/create_tool/get_lanlng_get_address/">http://scrap.php.xdomain.jp/create_tool/get_lanlng_get_address/</a></li>
  * </ul>
  * 
  * @author pro-tsato
  */
 public class LatLngUtility {
 	
-	/** 地球半径：6378.137 （出典：２地点間の距離と方位角 - 高精度計算サイト - http://keisan.casio.jp/exec/system/1257670779） */
+	/**
+	 * 地球半径：6378.137
+	 * <p>【参考サイト】
+	 * <ul>
+	 * <li>２地点間の距離と方位角 - 高精度計算サイト
+	 *  | <a href="http://keisan.casio.jp/exec/system/1257670779">http://keisan.casio.jp/exec/system/1257670779</a></li>
+	 * </ul>
+	 */
 	public static final double EARTH_DIAMETER = 6378.137;
 
 	/**
@@ -31,7 +38,7 @@ public class LatLngUtility {
 	 * <p>【参考サイト】
 	 * <ul>
 	 * <li>距離1kmあたりの緯度・経度の度数を計算（日本・北緯35度） | EasyRamble
-	 *  - http://easyramble.com/latitude-and-longitude-per-kilometer.html</li>
+	 *  | <a href="http://easyramble.com/latitude-and-longitude-per-kilometer.html">http://easyramble.com/latitude-and-longitude-per-kilometer.html</a></li>
 	 * </ul>
 	 * @param distance 距離（km）
 	 * @return 距離あたりの度数（緯度）
@@ -55,7 +62,7 @@ public class LatLngUtility {
 	 * <p>【参考サイト】
 	 * <ul>
 	 * <li>距離1kmあたりの緯度・経度の度数を計算（日本・北緯35度） | EasyRamble
-	 *  - http://easyramble.com/latitude-and-longitude-per-kilometer.html</li>
+	 *  | <a href="http://easyramble.com/latitude-and-longitude-per-kilometer.html">http://easyramble.com/latitude-and-longitude-per-kilometer.html</a></li>
 	 * </ul>
 	 * @param lat 緯度
 	 * @param distance 距離（km）
